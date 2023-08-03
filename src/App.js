14125d3f6ca7d26c2dcf641a604a913c68260e11
Anuj Jain <jainanuj744@gmail.com>
2023-08-03 07:20:03 +0530

    usecase of useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  let [name,setname] = useState("");
+
+  let inputChange = (e) => {
+    setname(e.target.value);
+  }
+
+  let clearInput = () => {
+    setname("");
+    hook.current.focus();
+
+  }
+
+  let colorChange = () => {
+    hook.current.style.color = "yellow";
+    hook.current.style.backgroundColor = "red";
+  }
+
+  let hook = useRef();
+  console.log(hook);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>UseRef</h2>
+      <input ref={hook} onChange={inputChange} value={name} type="text"></input>
+      <button onClick={clearInput}>Clear</button>
+      <button onClick={colorChange}>Color Change</button>
     </div>
   );
 }
